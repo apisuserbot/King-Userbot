81053387b5de4111fa015872c0e5a5f4bfe76c17
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-12 14:47:48 +0700

    Create help.py

---
+++ userbot/modules/help.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
+
+import redis
+import sys
+import platform
+import asyncio
+from asyncio import create_subprocess_exec as asyncrunapp
+from userbot import CMD_HELP, ALIVE_NAME, bot
+from userbot.events import register
+from sys import version_info
+from telethon import version
+from platform import uname
+from userbot import events
+
+modules = CMD_HELP
+
+# Ported by KENZO (Lynx-Userbot)
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("**`Maaf King Saya Tidak Punya Perintah Itu:)`**")
+            await asyncio.sleep(200)
+            await event.delete()
+    else:
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t|  "
+        await event.edit("**⚡King-Userbot⚡**\n\n"
+                         f"**◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◑» Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
+                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"╰►| {string} ◄─\n\n")
+        await event.reply(f"\n**🔥 Contoh** : Ketik <`.help afk`> Untuk Informasi Pengunaan.\nAtau Bisa Juga Ketik `.helpme` Untuk Main Menu Yang Lain-Nya. 🔥")
+        await asyncio.sleep(1000)
+        await event.delete()
