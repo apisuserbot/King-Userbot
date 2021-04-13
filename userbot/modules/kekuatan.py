77155b3e4da949b54a7d5a156d4849896912ad4a
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-14 05:11:08 +0700

    Create kekuatan.py

---
+++ userbot/modules/kekuatan.py
@@ -0,0 +1,171 @@
+
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# Original source for the deepfrying code (used under the following
+# license): https://github.com/Ovyerus/deeppyer
+
+# MIT License
+#
+# Copyright (c) 2017 Ovyerus
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# Ported from Xtra-telegram by @heyworld
+
+# KING USERBOT
+
+import os
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot.events import register
+from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
+
+
+@register(outgoing=True, pattern=r'^.kekuatan(:? |$)([1-8])?')
+async def _(fry):
+    await fry.edit("`King Mengaktifkan Kekuatan Telegram...⚡`")
+    level = fry.pattern_match.group(2)
+    if fry.fwd_from:
+        return
+    if not fry.reply_to_msg_id:
+        await fry.edit("`Mohon Balas Di Sticker King`")
+        return
+    reply_message = await fry.get_reply_message()
+    if not reply_message.media:
+        await fry.edit("`Gambar tidak di dukung`")
+        return
+    if reply_message.sender.bot:
+        await fry.edit("`Mohon Balas Di Sticker King`")
+        return
+    chat = "@image_deepfrybot"
+    message_id_to_reply = fry.message.reply_to_msg_id
+    async with fry.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/deepfry {level}"
+                msg_level = await conv.send_message(
+                    m,
+                    reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            else:
+                response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await fry.reply("`King Mohon Unblock` @image_deepfrybot`...`")
+            return
+        if response.text.startswith("Forward"):
+            await fry.edit("`King Mohon Matikan Setelan Forward Privasi...`")
+        else:
+            downloaded_file_name = await fry.client.download_media(
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
+            )
+            await fry.client.send_file(
+                fry.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply
+            )
+            """ - cleanup chat after completed - """
+            try:
+                msg_level
+            except NameError:
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
+            else:
+                await fry.client.delete_messages(
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
+    await fry.delete()
+    return os.remove(downloaded_file_name)
+
+
+@register(outgoing=True, pattern=r'^.df(:? |$)([1-8])?')
+async def _(fry):
+    await fry.edit("`Sedang Dalam Proses......`")
+    level = fry.pattern_match.group(2)
+    if fry.fwd_from:
+        return
+    if not fry.reply_to_msg_id:
+        await fry.edit("`Mohon Balas Di Sticker King`")
+        return
+    reply_message = await fry.get_reply_message()
+    if not reply_message.media:
+        await fry.edit("`Mohon Balas Di Sticker King`")
+        return
+    if reply_message.sender.bot:
+        await fry.edit("`Mohon Balas Di Sticker King`")
+        return
+    chat = "@image_deepfrybot"
+    message_id_to_reply = fry.message.reply_to_msg_id
+    async with fry.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/deepfry {level}"
+                msg_level = await conv.send_message(
+                    m,
+                    reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            else:
+                response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await fry.reply("`King Mohon Unblock` @image_deepfrybot`...`")
+            return
+        if response.text.startswith("Forward"):
+            await fry.edit("`King Mohon Matikan Setelan Privasi Forward...`")
+        else:
+            downloaded_file_name = await fry.client.download_media(
+                response.media,
+                TEMP_DOWNLOAD_DIRECTORY
+            )
+            await fry.client.send_file(
+                fry.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply
+            )
+            """ - cleanup chat after completed - """
+            try:
+                msg_level
+            except NameError:
+                await fry.client.delete_messages(conv.chat_id,
+                                                 [msg.id, response.id])
+            else:
+                await fry.client.delete_messages(
+                    conv.chat_id,
+                    [msg.id, response.id, r.id, msg_level.id])
+    await fry.delete()
+    return os.remove(downloaded_file_name)
+
+
+CMD_HELP.update({
+    "kekuatan":
+    "**Modules: __Kekuatan__\n\n⚡𝘾𝙈𝘿⚡: `.kekuatan` / `.kekuatan [level(1-8)]`"
+    "\n**Penjelasan:** untuk mengubah foto/sticker."
+})
