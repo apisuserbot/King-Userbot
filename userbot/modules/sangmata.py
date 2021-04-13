8e8ae56737fba3c779380f5fe7fa857c31dd52bf
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-13 11:26:35 +0700

    Create sangmata.py

---
+++ userbot/modules/sangmata.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Port to userbot by @MoveAngel
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import bot, CMD_HELP
+from userbot.events import register
+from asyncio.exceptions import TimeoutError
+
+
+@register(outgoing=True, pattern=r"^\.sa(?: |$)(.*)")
+async def lastname(steal):
+    if steal.fwd_from:
+        return
+    if not steal.reply_to_msg_id:
+        await steal.edit("```Mohon Balas Ke Pesan Pengguna King.```")
+        return
+    message = await steal.get_reply_message()
+    chat = "@SangMataInfo_bot"
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
+        await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
+        return
+    await steal.edit("```King Memerintahku Mengambil Informasi Riwayat Pergantian Nama Orang Ini```")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await steal.reply(
+                    "```King Mohon Unblock @sangmatainfo_bot Dan Coba Lagi```"
+                )
+                return
+            if r.text.startswith("Name"):
+                respond = await conv.get_response()
+                await steal.edit(f"`{r.message}`")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
+                )
+                return
+            if response.text.startswith("No records") or r.text.startswith(
+                "No records"
+            ):
+                await steal.edit("```Saya Tidak Menemukan Informasi Pergantian Nama, King Orang Ini Belum Pernah Mengganti Namanya```")
+                await steal.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await steal.edit(f"```{response.message}```")
+            await steal.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await steal.edit("`Saya Sedang Sakit King Maaf:(`")
+
+
+CMD_HELP.update({
+    "sangmata":
+        "`.sa`\
+          \nUsage: Mendapatkan Riwayat Nama Pengguna."
+})
