a4aada611a147a9184e60eb02e85fb87069077f9
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-13 13:34:11 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -180,7 +180,7 @@ S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 
 # Default .alive logo
 ALIVE_LOGO = os.environ.get(
-    "ALIVE_LOGO") or "https://telegra.ph/file/62dc59b2013a48f9cc8f3.jpg"
+    "ALIVE_LOGO") or "https://telegra.ph/file/1bf2eacc0f1aaca83eaa5.jpg"
 
 # Last.fm Module
 BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
