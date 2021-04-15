55b4e782ca50f8935b9ddb7c136d418881787f50
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-04-15 11:21:12 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# System Stats Lord-Userbot
+# System Stats King-Userbot
 
 import asyncio
 from asyncio import create_subprocess_exec as asyncrunapp
