fc8a8b2716fda36f18735a1d0820eab5ebb26f02
apisuserbot <nafisqurtubi154@gmail.com>
2021-04-19 01:50:16 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -296,7 +296,7 @@ async def amireallyalive(alive):
 
 @register(outgoing=True, pattern=r"^\.(?:team|teamon)\s?(.)?")
 async def amireallyalive(alive):
-    user = await bot.get_me()
+    await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     await alive.edit("..⚡𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡..")
     output = (
