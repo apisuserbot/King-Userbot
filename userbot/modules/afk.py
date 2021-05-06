4df780bd5f25110574f8820f7fc919bca48ceeac
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-05-06 17:23:23 +0700

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -98,7 +98,7 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("**✘ 𝗞𝗶𝗻𝗴 `kembali ada yang kangen gak?`✘**")
+        msg = await notafk.respond("**✘ King `Telah kembali ada yang kangen gak?...`✘**")
         time.sleep(3)
         await msg.delete()
         await notafk.client(UpdateProfileRequest(first_name=user.first_name, last_name=last1))
