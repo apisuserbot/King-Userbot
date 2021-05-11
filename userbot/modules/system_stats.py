7b86706d6378a336157dadd3d719905d258b6aba
apisuserbot <nafisqurtubi154@gmail.com>
2021-05-11 01:49:49 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -281,9 +281,9 @@ async def amireallyalive(alive):
         f" • KING BY : `{DEFAULTUSER}`"
     if ALIVE_LOGO:
         try:
-            logo = ALIVE_LOGO
+            logo=ALIVE_LOGO
             await alive.delete()
-            msg = await bot.send_file(alive.chat_id, logo, caption=output)
+            msg=await bot.send_file(alive.chat_id, logo, caption=output)
             await asyncio.sleep(200)
             await msg.delete()
         except BaseException:
@@ -299,9 +299,9 @@ async def amireallyalive(alive):
         await alive.delete()
 
 
-@register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
+@ register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def redis(alive):
-    user = await bot.get_me()
+    user=await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     await alive.edit("__Connecting to server.__")
     await alive.edit("__Connecting to server..__")
@@ -346,7 +346,7 @@ async def redis(alive):
     await asyncio.sleep(3)
     await alive.edit("😼")
     await asyncio.sleep(3)
-    output = (
+    output=(
         f"**ㅤㅤ  ╭─━━═━═━═━═━━─╮** \n"
         f"**       ⊏┊⚡𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡ ⊨〛💨 ** \n"
         f"**ㅤㅤ  ╰─━━═━═━═━═━━─╯** \n"
@@ -368,9 +368,9 @@ async def redis(alive):
         f"[𝗞𝗶𝗻𝗴](t.me/PacarFerdilla) | ")
     if ALIVE_LOGO:
         try:
-            logo = ALIVE_LOGO
+            logo=ALIVE_LOGO
             await alive.delete()
-            msg = await bot.send_file(alive.chat_id, logo, caption=output)
+            msg=await bot.send_file(alive.chat_id, logo, caption=output)
             await asyncio.sleep(500)
             await msg.delete()
         except BaseException:
@@ -386,23 +386,23 @@ async def redis(alive):
         await alive.delete()
 
 
-@register(outgoing=True, pattern="^.aliveu")
+@ register(outgoing=True, pattern="^.aliveu")
 async def amireallyaliveuser(username):
     """ For .aliveu command, change the username in the .alive command. """
-    message = username.text
-    output = ".aliveu [new username] tidak boleh kosong"
+    message=username.text
+    output=".aliveu [new username] tidak boleh kosong"
     if not (message == ".aliveu" and message[7:8] != " "):
-        newuser = message[8:]
+        newuser=message[8:]
         global DEFAULTUSER  # global statement
-        DEFAULTUSER = username
-        output = "Successfully changed user to " + newuser + "!"
+        DEFAULTUSER=username
+        output="Successfully changed user to " + newuser + "!"
     await username.edit("`" f"{output}" "`")
 
 
-@register(outgoing=True, pattern=r"^\.resetalive$")
+@ register(outgoing=True, pattern=r"^\.resetalive$")
 async def amireallyalivereset(ureset):
     global DEFAULTUSER  # global statement
-    DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+    DEFAULTUSER=str(ALIVE_NAME) if ALIVE_NAME else uname().node
     await ureset.edit("`" "Successfully reset user for alive!" "`")
 
 
