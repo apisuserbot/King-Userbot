5dcabdcc0e58d387ce92c69d2e4b6af074dc440c
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-05-26 04:56:43 +0700

    Mengubah await dan help
    await dan plugin

--- userbot/modules/hentai.py
+++ userbot/modules/hentai.py
@@ -1,6 +1,14 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module for filter commands """
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+
+from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
@@ -10,25 +18,29 @@ async def _(event):
         return
     link = event.pattern_match.group(1)
     chat = "@nHentaiBot"
-    await event.edit("```Processing```")
+    await event.edit("```Sedang Memproses```")
     async with bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
-                events.NewMessage(
-                    incoming=True,
-                    from_users=424466890))
+                events.NewMessage(incoming=True, from_users=424466890)
+            )
             await bot.send_message(chat, link)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock @nHentaiBot and try again```")
+            await event.reply("```Harap buka blokir @nHentaiBot dan coba lagi```")
             return
-        if response.text.startswith("**Sorry I couldn't get manga from**"):
-            await event.edit("```I think this is not the right link```")
+        if response.text.startswith("**Maaf, saya tidak bisa mendapatkan manga dari**"):
+            await event.edit("```Saya pikir ini bukan tautan yang tepat king!```")
         else:
             await event.delete()
             await bot.send_message(event.chat_id, response.message)
 
-CMD_HELP.update({
-    "hentai":
-    "`.hentai` <link / code> \
-\nUsage: view nhentai in telegra.ph XD\n"})
+
+CMD_HELP.update(
+    {
+        "hentai": "**✘ Plugin : **`hentai`\
+        \n\n  •  **Perintah :** `.hentai`\
+        \n  •  **Function : **Melihat nhentai di telegra.ph XD\
+    "
+    }
+)
