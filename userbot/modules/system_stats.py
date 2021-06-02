21d7fffd64a9becab304561d4df7e3edecfc4d9c
apisuserbot <nafisqurtubi154@gmail.com>
2021-06-02 11:30:34 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -393,7 +393,7 @@ async def redis(alive):
 
 @register(outgoing=True, pattern=r"^\.(?:ualive|uon)\s?(.)?")
 async def amireallyalive(alive):
-    user = await bot.get_me()
+    await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     await alive.edit("`★ USERBOT HAS BEEN ACTIVE! ★`")
     await asyncio.sleep(3)
