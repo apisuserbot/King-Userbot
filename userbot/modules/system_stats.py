87ca3e455db593ed765f2c1de7e17732a4e7d44f
apisuserbot <nafisqurtubi154@gmail.com>
2021-06-02 09:12:27 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Ported and bug fixes by Apis 
+# Ported and bug fixes by Apis
 """ Userbot module for getting information about the server. """
 
 
