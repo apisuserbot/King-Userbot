f997afabf25b900116d0b309786036257c2fdf22
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-05 13:11:47 +0700

    Update init
    • Ada bug

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -378,7 +378,7 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "⌫", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "☒", data="{}_close({})".format(prefix, module_page)
+                    '☒', b'close'
                 ),
                 custom.Button.inline(
                     "⌦", data="{}_next({})".format(prefix, modulo_page)
