83aee24db06f9029c2ff9146b790bcb7334c2373
apisuserbot <nafisqurtubi154@gmail.com>
2021-06-06 23:07:42 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/modules/costum.py
+++ userbot/modules/costum.py
@@ -12,7 +12,10 @@ from userbot import BOTLOG_CHATID, CMD_HELP
 """ Userbot module containing commands for keeping costum global notes. """
 
 
-@register(outgoing=True, pattern=r"\.\w*", ignore_unsafe=True,disable_errors=True)
+@register(outgoing=True,
+          pattern=r"\.\w*",
+          ignore_unsafe=True,
+          disable_errors=True)
 async def on_snip(event):
     """costums logic."""
     try:
