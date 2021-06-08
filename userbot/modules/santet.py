bea05ad0aead708bb5c0a6f89706789963868b9c
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-08 10:13:13 +0700

    Update santet
    • Edit by Apis

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -313,14 +313,42 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online,Maap Cuman Bercanda:v`")
+    await typew.edit("`Target Sukses Tersantet Online,Maap Cuman Bercanda:v`")
 
+
+@register(outgoing=True, pattern="^.psikopat(?: |$)(.*)")
+async def typewriter(typew):
+    typewriter.pattern_match.group(1)
+    await typew.edit("`Mau gw bunuh loe?`)
+    sleep(3)
+    await typew.edit("`Sini share lock kalo berani`")
+    sleep(3)
+    await typew.edit("`Okelah kalo emang gak berani`")
+    sleep(3)
+    await typew.edit("`Gw yang kesana , tunggu loe...`")
+    sleep(2)
+    await typew.edit("`Saatnya beraksi...`")
+    sleep(1)
+    await typew.edit("`10`℅")
+    await typew.edit("`20`℅")
+    await typew.edit("`30`℅")
+    await typew.edit("`40`℅")
+    await typew.edit("`50`℅")
+    await typew.edit("`60`℅")
+    await typew.edit("`70`℅")
+    await typew.edit("`80`℅")
+    await typew.edit("`90`℅")
+    await typew.edit("`100`℅")
+    sleep(1)
+    await typew.edit("`Tapi boong hayuk palepalepale:v`")
 
 CMD_HELP.update(
     {
-        "santet": "**✘ Plugin :** `santet\
+        "santet": "**✘ Plugin :** `santet`\
         \n\n  •  **Perintah :** `.santet`\
         \n  •  **Function : **Santet Online Buat Bercanda Doang:)\
+        \n\n  •  **Perintah :** `.psikopat`\
+        \n  •  **Function : **Psikopat boongan:v\
     "
     }
 )
