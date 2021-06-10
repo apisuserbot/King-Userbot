2c5ef9850fcd21740db2fc73c5f7fad8317d039b
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-10 11:23:42 +0700

    Update gcast
    • ©Copyright King-Userbot (Apis)
    Ported by Apis

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,21 +1,19 @@
-# frm Ultroid
-# port by Koala @manusiarakitann
-# @LordUserbot_Group
-# Apis Ganteng
+# ©Copyright King-Userbot (Apis) USERBOT TELEGRAM 
+# Ported by Apis
+# Thanks for ultroid 
 
 from userbot.events import register
 from userbot import CMD_HELP, bot
-# Apis Ganteng
 
 
 @register(outgoing=True, pattern="^.gcast (.*)")
 async def gcast(event):
-    xx = event.pattern_match.group(1)
-    if not xx:
+    kinguser = event.pattern_match.group(1)
+    if not kinguser:
         return await event.edit("`King, Mohon Berikan Sebuah Pesan`")
     tt = event.text
     msg = tt[6:]
-    kk = await event.edit("`Sedang Mengirim Pesan Secara Global...⚡️`")
+    kingget = await event.edit("`Sedang Mengirim Pesan Secara Global...⚡️`")
     er = 0
     done = 0
     async for x in bot.iter_dialogs():
@@ -26,14 +24,14 @@ async def gcast(event):
                 await bot.send_message(chat, msg)
             except BaseException:
                 er += 1
-    await kk.edit(f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
+    await kingget.edit(f"**Sukses Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
 
-# Apis Ganteng
+
 CMD_HELP.update(
     {
         "gcast": "**✘ Plugin : **`gcast`\
         \n\n  •  **Perintah :** `.gcast` <text>`\
-        \n  •  **Function : **Mengirim  Global Broadcast pesan ke Seluruh Grup yang king masuki.\
+        \n  •  **Function : **Mengirim  Global Broadcast pesan ke Seluruh Grup yang king masuki\
     "
     }
 )
