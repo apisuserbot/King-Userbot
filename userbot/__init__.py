575f31e2538af3ef4c7d550ea43e2ab3cceabdd7
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-13 09:37:54 +0700

    Update init
    • Modifikasi manu close (penutup)

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -383,6 +383,9 @@ def paginate_help(page_number, loaded_modules, prefix):
                     "⌫", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
+                    "☒", data="{}_close({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline(
                     "⌦", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
