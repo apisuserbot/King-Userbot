b8c0ef78e8b8d2d13b0da77d06f3e3ca90aff208
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-13 08:44:38 +0700

    Opss...
    • Maaf kesalahan sedikit

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -208,7 +208,7 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond(**Perintah Untuk Update ** ⚡𝗞𝗶𝗻𝗴 - 𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡\n• ** Perintah ** `.update now`\n• ** Perintah ** `.update deploy`\n\n__Untuk Meng Update Fitur Terbaru Dari ⚡𝗞𝗶𝗻𝗴 - 𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡.__')
+        return await event.respond('**Perintah Untuk Update** ⚡𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡\n•  ** Perintah ** `.update now`\n•  ** Perintah ** `.update deploy`\n\n__Untuk Meng Update Fitur Terbaru Dari ⚡𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡.__')
 
     if force_update:
         await event.edit(
