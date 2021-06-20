88fa4a6641cd3caea17095bd890946684a301283
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-20 17:05:21 +0700

    Add files info_akun.py
    • F I X E S created and ported

---
+++ userbot/modules/info_akun.py
@@ -0,0 +1,35 @@
+# Ported info by Apis
+# Thanks Ultroid limited
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.info(?: |$)(.*)")
+async def _(event):
+    await event.edit("`Mengecek Info Akun Anda...`")
+    async with bot.conversation("@SpamBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178220800)
+            )
+            await conv.send_message("/start")
+            response = await response
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("`Harap unblock @SpamBot dan coba lagi`")
+            return
+        await event.edit(f"• Info Akun :\n{response.message.message}")
+
+
+CMD_HELP.update(
+    {
+        "info": "**✘ Plugin :** `info akun`\
+        \n\n  •  **Perintah :** `.info`\
+        \n  •  **Function : **Untuk mengecek info akun Anda\
+    "
+    }
+)
