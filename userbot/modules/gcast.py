68254698d7e5290a079bb80c508e0280df44662a
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-21 05:56:39 +0700

    F I X E S

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,4 +1,4 @@
-# ©Copyright King-Userbot (Apis) USERBOT TELEGRAM
+# (c) Copyright King-Userbot (Apis) USERBOT TELEGRAM
 # Ported by Apis
 # Thanks for ultroid
 # Thanks for ᴀxᴇʟ.ᴀʟ/ᴄᴏɴᴛʀɪʙᴜᴛᴏʀ
@@ -25,17 +25,17 @@ async def gcast(event):
                 await bot.send_message(chat, msg)
             except BaseException:
                 er += 1
-    await kingget.edit(f"**Sukses Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
+    await kingget.edit(f"**Sukses Mengirim Pesan Ke** `{done}` **Grup , Gagal Mengirim Pesan Ke** `{er}` **Grup**")
 
 
-@register(outgoing=True, pattern="^.gucast (.*)")
+@register(outgoing=True, pattern="^.pcast (.*)")
 async def gucast(event):
     kinguser = event.pattern_match.group(1)
     if not kinguser:
         return await event.edit("`Mohon Berikan Sebuah Pesan`")
     tt = event.text
     msg = tt[7:]
-    kingget = await event.edit("`Sedang Mengirim Pivate Messages Secara Global...⚡️`")
+    kingget = await event.edit("`Sedang Mengirim Pesan Pribadi Secara Global...⚡️`")
     er = 0
     done = 0
     async for x in bot.iter_dialogs():
@@ -46,16 +46,16 @@ async def gucast(event):
                 await bot.send_message(chat, msg)
             except BaseException:
                 er += 1
-    await kingget.edit(f"**Sukses Mengirim Pesan Ke** `{done}` **Orang, Gagal Mengirim Pesan Ke** `{er}` **Orang.**")
+    await kingget.edit(f"**Sukses Mengirim Pesan Ke** `{done}` **Orang , Gagal Mengirim Pesan Ke** `{er}` **Orang.**")
 
 
 CMD_HELP.update(
     {
         "gcast": "**✘ Plugin : **`Global Broadcast`\
         \n\n  •  **Perintah :** `.ggcast` <Text>`\
-        \n  •  **Function : **Global Group Broadcast. Mengirim Pesan Global Broadcast pesan ke Seluruh Grup Yang King Masuki\
-        \n\n  •  **Perintah :** `.gucast` <Text>`\
-        \n  •  **Function : **Global Users Broadcast. Kirim Pesan itu Secara Global ke Semua Anggota Group Anda\
+        \n  •  **Function : **Global Group Broadcast , Mengirim Pesan Global Broadcast pesan ke Seluruh Grup Yang King Masuki\
+        \n\n  •  **Perintah :** `.pcast` <Text>`\
+        \n  •  **Function : **Global Pengguna Broadcast , Kirim Pesan itu Secara Global ke Semua Pengguna Atau Anggota Grup Anda\
      "
     }
 )
