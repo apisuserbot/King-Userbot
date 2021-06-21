7e6ac9bf49524862f5696b2b3fa8108a85a6479e
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-21 16:21:19 +0700

    .

--- userbot/modules/fakeaction.py
+++ userbot/modules/fakeaction.py
@@ -103,13 +103,67 @@ async def _(event):
         f"◈ **KING :** {ALIVE_NAME} \n"
         f"◈ **REPO :** [King-Userbot](https://github.com/apisuserbot/King-Userbot)")
 
+
+@register(outgoing=True, pattern="^.fgban(?: |$)(.*)")
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        if r.forward:
+            r_from_id = r.forward.from_id or r.from_id
+        else:
+            r_from_id = r.from_id
+        await bot.send_message(
+            G_BAN_LOGGER_GROUP,
+            "/gban [user](tg://user?id={}) {}".format(r_from_id, reason)
+        )
+    await event.delete()
+    await event.reply("**gbanning...**")
+    asyncio.sleep(3.5)
+    await event.edit(f"**User gbanned by {DEFAULTUSER}**")
+    asyncio.sleep(5)
+    await event.delete()
+
+
+@register(outgoing=True, pattern="^.fungban(?: |$)(.*)")
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("Set G_BAN_LOGGER_GROUP in vars otherwise module won't work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        r_from_id = r.from_id
+        await bot.send_message(
+            G_BAN_LOGGER_GROUP,
+            "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason)
+        )
+    await event.delete()
+    await event.reply("**ungbanning...**")
+    asyncio.sleep(3.5)
+    await event.edit(f"**User ungbanned by {DEFAULTUSER}**")
+    asyncio.sleep(5)
+    await event.delete()
+
+
 CMD_HELP.update(
     {
         "fakeaction": "**✘ Plugin :** `fakeaction`\
         \n\n  •  **Perintah :** `.ftyping` | `.faudio` | `.fvideo` | `.fgame` <jumlah text>\
         \n  •  **Function : **Fake Ini Yang Bisa Menipu Saat Anda Mengetik , Audio , Video , Memainkan Game Selama Mungkin\
         \n\n  •  **Perintah :** `.fkuota`\
-        \n  •  **Function : **Fake dyno Yang Bisa Menipu Saat Anda Mengetik `.fkuota` Yang Keluar 1000 Jam Dyno , Selamat Mencoba\
+        \n  •  **Function : **Fake dyno 1000 jam\
+        \n\n  •  **Perintah :** `.fgban`\
+        \n  •  **Function : **Fake global banned\
+        \n\n  •  **Perintah :** `.fungban`\
+        \n  •  **Function : **Fake unglobal banned\
     "
     }
 )
