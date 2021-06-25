5a82e84c8b02a118b6a794209dd6f1a083446d62
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-06-25 10:11:14 +0700

    fixed

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -38,8 +38,8 @@ async def help(event):
                          f"**◑» King** {DEFAULTUSER}\n**◑» Plugins :** `{len(modules)}`\n\n"
                          "**❒ Main Menu ↯**\n"
                          f"╰►| {string} ◄─\n\n"
-                         f"**LICENSE :** [GNU GPL v3.0](https://github.com/apisuserbot/King-Userbot/blob/King-Userbot/LICENSE)\n"
-                         f"**Copyright (C) 2021 King-Userbot USERBOT TELEGRAM**")
+                         "**LICENSE :** [GNU GPL v3.0](https://github.com/apisuserbot/King-Userbot/blob/King-Userbot/LICENSE)\n"
+                         "**Copyright (C) 2021 King-Userbot USERBOT TELEGRAM**")
         await event.reply(f"\n**◑» Perintah Plugin**\n\n**Contoh** : **Ketik** `.help afk` **Untuk Informasi Pengunaan.\nAtau Bisa Juga Ketik** `.helpme` **Untuk Main Menu Yang Lain-Nya.**")
         await asyncio.sleep(1000)
         await event.delete()
