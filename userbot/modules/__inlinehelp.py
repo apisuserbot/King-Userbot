926f4cfcd3caa8f0703c6ec3641f40ba443f8ffa
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-03 11:53:53 +0700

    fixes

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -13,8 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# @Qulec tarafından yazılmıştır.
-# Thanks @Spechide.
+# Thanks @Spechide
 
 import logging
 
@@ -32,16 +31,16 @@ async def yardim(event):
     try:
         tgbotusername = BOT_USERNAME
         if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@UserButt")
+            results = await event.client.inline_query(tgbotusername, "@KingUserbotSupport")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
         else:
             await event.edit(
-                "`The bot doesn't work! Please set the Bot Token and Username correctly. The module has been stopped.`"
+                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username di Bot father dengan benar, Plugin telah dihentikan`"
             )
     except Exception:
         return await event.edit(
-            "`You cannot send inline results in this chat (caused by SendInlineBotResultRequest)`"
+            "`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)`"
         )
