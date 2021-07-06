ef9235c64cdd11b581318b5bd25318b75ed933c8
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-06 16:28:04 +0700

    Update botver
    • Versi King : 0.5.2 to 0.5.3

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -191,7 +191,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "King-Userbot")
 
 # Bot version
-BOT_VER = os.environ.get("BOT_VER", "0.5.2")
+BOT_VER = os.environ.get("BOT_VER", "0.5.3")
 
 # Default .alive username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
