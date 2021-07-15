5b7aa7d3ffff4328dd2527ddf73d03cbe51caeed
apisuserbot <nafisqurtubi154@gmail.com>
2021-07-15 03:15:23 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -438,15 +438,15 @@ with king:
                     f"I was **Created by :** @PacarFerdilla For Various Userbots on Github\n")
                 await king.tgbot.send_file(event.chat_id, logo,
                                            caption=text,
-                                      buttons=[
-                                          [
-                                              custom.Button.url(
-                                                  text="Support Chat",
-                                                  url="https://t.me/KingUserbotSupport"
-                                              )
-                                          ]
-                                      ]
-                                      )
+                                           buttons=[
+                                               [
+                                                   custom.Button.url(
+                                                       text="Support Chat",
+                                                       url="https://t.me/KingUserbotSupport"
+                                                   )
+                                               ]
+                                           ]
+                                           )
 
         @king.tgbot.on(events.InlineQuery)  # pylint:disable=E0602
         async def inline_handler(event):
