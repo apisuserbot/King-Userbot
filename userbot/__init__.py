41cdabd011eaa9ba11f60c0da6438988a510d141
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-23 13:11:33 +0700

    ...

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -375,9 +375,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline(
-    "{} {} |".format(
-        "|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -570,6 +568,9 @@ with king:
         @king.tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid:
+                buttons = [
+                    (custom.Button.inline("Buka Kembali", data="opener"),),
+                ]
                 await event.edit("__**- Help Button Ditutup -**__")
             else:
                 reply_pop_up_alert = f"🔒 Code Tersembunyi 🔒\n\nUserbot Milik {ALIVE_NAME} Yang Hanya Bisa Melihat Code Tersembunyi"
