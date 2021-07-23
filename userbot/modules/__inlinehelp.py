e9afa6f943555a031e3312f6ee124a87e7502e3e
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-23 07:12:05 +0700

    fixes: inline

--- userbot/modules/__inlinehelp.py
+++ userbot/modules/__inlinehelp.py
@@ -29,9 +29,9 @@ logging.basicConfig(
 @register(outgoing=True, pattern=r"^\.helpme")
 async def yardim(event):
     try:
-        tgbotusername = BOT_USERNAME
-        if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@KingUserbotSupport")
+        kingbotusername = BOT_USERNAME
+        if kingbotusername is not None:
+            results = await event.client.inline_query(kingbotusername, "@KingUserbotSupport")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
