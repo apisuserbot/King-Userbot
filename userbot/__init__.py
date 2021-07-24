390e0da0ca8d97352bb8db682a4caa90ca1a309f
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-24 07:23:47 +0700

    fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -377,9 +377,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline(
-    "{} {} |".format(
-        "|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -588,9 +586,9 @@ with king:
                                        "t.me/KingUserbotSupport")],
                            [Button.url("Donasi Developer",
                                        "t.me/PacarFerdilla")],
-                        [Button.inline("Buka Menu", data="opener")],
+                        [custom.Button.inline("Buka Menu", data="opener")],
                         [custom.Button.inline(
-                            "Tutup", b"close")],
+                            "☒", b"close")],
                     ]
                 )
              else:
@@ -614,6 +612,13 @@ with king:
             else:
                 reply_pop_up_alert = f"🔒 Code Tersembunyi 🔒\n\nUserbot Milik {ALIVE_NAME} Yang Hanya Bisa Melihat Code Tersembunyi"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+        @king.tgbot.on(events.CallbackQuery(data=b"close"))
+        async def close(event):
+            buttons = [
+                (custom.Button.inline("Buka Menu", data="opener"),),
+            ]
+            await event.edit("Menu Ditutup", file=logo, buttons=buttons)
 
         @king.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
