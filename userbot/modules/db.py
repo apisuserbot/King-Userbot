79e40018300ecc89552773d6ef2c27816a1074b3
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-07-25 13:47:22 +0700

    fixes: db

--- userbot/modules/db.py
+++ userbot/modules/db.py
@@ -10,6 +10,7 @@ from platform import uname
 
 from userbot import ALIVE_NAME, BOT_VER, is_mongo_alive, is_redis_alive
 from userbot.events import register
+from userbot import CMD_HELP
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
@@ -19,15 +20,27 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.db$")
 async def amireallydbs(dbs):
     if not is_mongo_alive() and not is_redis_alive():
-        db = "Both Mongo and Redis Database seems to be failing!"
+        db = "Bot Mongo dan Redis Database sepertinya gagal!"
     elif not is_mongo_alive():
-        db = "Mongo DB seems to be failing!"
+        db = "Mongo DB sepertinya gagal!"
     elif not is_redis_alive():
-        db = "Redis Cache seems to be failing!"
+        db = "Redis Cache sepertinya gagal!"
     else:
         db = "Databases functioning normally!"
     await dbs.edit(""
-                   f"**User:** `{DEFAULTUSER}` \n"
-                   f"**Status Database:** `{db}`\n"
-                   f"**King-Userbot:** `{BOT_VER}`"
+                   f"**Database Mongo db** \n\n"
+                   f"**Pengguna :** `{DEFAULTUSER}` \n"
+                   f"**Status Database :** `{db}`\n"
+                   f"**Branch :** __King-Userbot__\n"
+                   f"**Versi Userbot :** `{BOT_VER}`"
                    "")
+
+
+CMD_HELP.update(
+    {
+        "db": "**✘ Plugin :** `Db`\
+        \n\n  •  **Perintah :** `.db`\
+        \n  •  **Function : **Mengecek Database Mongo db\
+    "
+    }
+)
