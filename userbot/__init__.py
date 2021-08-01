ff23eff452a9f4e4084c79d92d78825241074805
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-01 17:07:23 +0700

    ...

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,9 +382,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline(
-    "{} {} |".format(
-        "|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -556,7 +554,7 @@ with king:
                        f"`Pengguna  :` {DEFAULTUSER} \n"
                        f"`Branch    :` {BRANCH} \n"
                        f"`Versi Bot :` {BOT_VER} \n"
-                       f"`Python    :` v.{python_version} \n"
+                       f"`Bahasa    :` Python  \n"
                        f"================================== "
                 await event.edit(
                     text,
@@ -662,17 +660,22 @@ with king:
         @king.tgbot.on(events.CallbackQuery(data=b"close"))
         async def close(event):
             if event.query.user_id == uid:
-                buttons = [
-                    [custom.Button.inline("Buka Menu", data="opener")],
-                    [custom.Button.inline("Alive", data="alive_inline")],
-                ]
+                text = f"\n**Menu Ditutup!**"
+                await event.edit(
+                    text,
+                    file=logo,
+                    link_preview=True,
+                    buttons=[
+                        [custom.Button.inline("Buka Menu", data="opener")],
+                        [custom.Button.inline(
+                            "Alive", data="alive_inline")],
+                    ]
                 )
-                await event.edit("**Menu Ditutup**", file = logo, buttons = buttons)
             else:
-                reply_pop_up_alert=f"❌ DISCLAIMER ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini"
+                reply_pop_up_alert = f"❌ DISCLAIMER ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini"
                 await event.answer(reply_pop_up_alert, cache_time = 0, alert = True)
 
-        @ king.tgbot.on(
+        @king.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(b"ub_modul_(.*)")
             )
@@ -683,17 +686,16 @@ with king:
 
                 cmdhel=str(CMD_HELP[modul_name])
                 if len(cmdhel) > 150:
-                    help_string=(
-                        str(CMD_HELP[modul_name]).replace(
-                            '`', '')[:150] + "..."
+                    help_string = (
+                        str(CMD_HELP[modul_name]).replace('`', '')[:150] + "..."
                         + "\n\nBaca Teks Berikutnya Ketik .help "
                         + modul_name
                         + " "
                     )
                 else:
-                    help_string=str(CMD_HELP[modul_name]).replace('`', '')
+                    help_string = str(CMD_HELP[modul_name]).replace('`', '')
 
-                reply_pop_up_alert=(
+                reply_pop_up_alert = (
                     help_string
                     if help_string is not None
                     else "{} Tidak ada dokumen yang ditulis dari plugin.".format(
@@ -701,7 +703,7 @@ with king:
                     )
                 )
             else:
-                reply_pop_up_alert=f"🔒 Code Tersembunyi 🔒\n\nUserbot Milik {ALIVE_NAME} Yang Hanya Bisa Melihat Code Tersembunyi"
+                reply_pop_up_alert = f"🔒 Code Tersembunyi 🔒\n\nUserbot Milik {ALIVE_NAME} Yang Hanya Bisa Melihat Code Tersembunyi"
 
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
