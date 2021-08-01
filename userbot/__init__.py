b463b10a98823a68feaa99994165835e10c9ce5e
apisuserbot <nafisqurtubi154@gmail.com>
2021-08-01 09:54:34 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,7 +382,9 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline(
+    "{} {} |".format(
+        "|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -660,17 +662,17 @@ with king:
         @king.tgbot.on(events.CallbackQuery(data=b"close"))
         async def close(event):
             if event.query.user_id == uid:
-                buttons=[
+                buttons = [
                     [custom.Button.inline("Buka Menu", data="opener")],
                     [custom.Button.inline("Alive", data="alive_inline")],
                 ]
                 )
-                await event.edit("**Menu Ditutup**", file=logo, buttons=buttons)
+                await event.edit("**Menu Ditutup**", file = logo, buttons = buttons)
             else:
-                reply_pop_up_alert = f"❌ DISCLAIMER ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini"
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+                reply_pop_up_alert=f"❌ DISCLAIMER ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini"
+                await event.answer(reply_pop_up_alert, cache_time = 0, alert = True)
 
-        @king.tgbot.on(
+        @ king.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(b"ub_modul_(.*)")
             )
