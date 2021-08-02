639faa7b90a050e7fc5842cff44825b71ce04962
apisuserbot <nafisqurtubi154@gmail.com>
2021-08-02 14:40:59 +0000

    pylint: auto fixes
    Signed-off-by: apisuserbot <nafisqurtubi154@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -459,13 +459,13 @@ with king:
                                                    custom.Button.url(
                                                        text="Tambahkan Bot Ini Ke Grup",
                                                        url=f"http://t.me/{BOT_USERNAME}?startgroup=true")],
-                                                  [custom.Button.url(
-                                                       text="Support Chat",
-                                                       url="https://t.me/KingUserbotSupport"),
+                                               [custom.Button.url(
+                                                   text="Support Chat",
+                                                   url="https://t.me/KingUserbotSupport"),
                                                    custom.Button.url(
                                                        text="Support Channel",
                                                        url="https://t.me/TeamKingUserbot"
-                                                   )
+                                               )
                                                ]
                                            ]
                                            )
