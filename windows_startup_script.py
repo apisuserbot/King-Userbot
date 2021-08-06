ff756ccfb271c18717b87fbd240e12d53fb49128
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-07 05:53:53 +0700

    run

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient
-API_KEY = "Type that here"
-API_HASH = "Type that here"
+API_KEY = "Harap Anda Memasukan API_KEY Dengan Benar"
+API_HASH = "Harap Anda Memasukan API_HASH Dengan Benar"
 # get it from my.telegram.org
 bot = TelegramClient('userbot', API_KEY, API_HASH)
 bot.start()
