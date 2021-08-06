b04958b301e1ffda822f1dd253e75249448d4d87
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-06 15:36:13 +0700

    ...

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,9 +382,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline(
-    "{} {} |".format(
-        "|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -681,7 +679,8 @@ with king:
                     link_preview=True,
                     buttons=[
                         [
-                            custom.Button.url("Support Chat", "https: // t.me / KingUserbotSupport),
+                            custom.Button.url("Support Chat", 
+                                              "https://t.me/KingUserbotSupport"),
                             custom.Button.url("Support Channel",
                                               "https://t.me/TeamKingUserbot"),
                             custom.Button.url("Donasi Developer",
@@ -698,14 +697,14 @@ with king:
                 reply_pop_up_alert= f"❌ DISCLAIMER ❌\n\nAnda Tidak Mempunyai Hak Untuk Menekan Tombol Button Ini"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-        @ king.tgbot.on(
+        @king.tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"alive_inline")
             )
         )
         async def on_plug_in_callback_query_handler(event):
             if event.query.user_id == uid:
-                text= (
+                text = (
                     f"__**{KING_TEKS_KUSTOM}**__ \n\n"
                     "==================================\n"
                     f"           **Alive Inline** \n"
