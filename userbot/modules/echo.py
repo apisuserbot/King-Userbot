e6e5f0afed556472eb353a7f2c3e2652fcc5a719
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-09 19:14:15 +0700

    ...

--- userbot/modules/echo.py
+++ userbot/modules/echo.py
@@ -3,6 +3,7 @@
 
 from userbot import CMD_HELP, bot
 from userbot.events import register
+from telethon import events
 
 from userbot.utils import edit_delete, edit_or_reply
 from userbot.modules.sql_helper.echo_sql import (
