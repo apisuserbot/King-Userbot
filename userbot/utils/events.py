ce5938c4fa7cdd483749a8de20cfdbf5b84e0563
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-09 18:40:01 +0700

    dahlah

--- userbot/utils/events.py
+++ userbot/utils/events.py
@@ -1,7 +1,7 @@
 from telethon.tl.types import MessageEntityMentionName
 
 from userbot.core.logger import logging
-from utils.tools import edit_delete
+from userbot.utils import edit_delete
 
 LOGS = logging.getLogger("userbot")
 
