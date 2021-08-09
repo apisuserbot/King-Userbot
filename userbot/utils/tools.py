6348710a971b8d4a527ddb3ee630e595227294bf
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-09 16:47:47 +0700

    ...

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -12,7 +12,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# 
 # Fixes by Apis
 
 
@@ -32,7 +33,7 @@ from PIL import Image
 from typing import Optional
 
 from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator, DocumentAttributeFilename
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator, DocumentAttributeFilename, MessageEntityMentionName
 from userbot import SUDO_USERS
 
 
