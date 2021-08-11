f7e6193efb3a767a79747803de103a909475180d
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-11 14:46:41 +0700

    fixes

--- userbot/modules/kinghelper.py
+++ userbot/modules/kinghelper.py
@@ -11,33 +11,29 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.khelp$")
 async def usit(e):
     await e.edit(
-        f"      ╔════════════╗\n     ⚡️𝘽𝘼𝙉𝙏𝙐𝘼𝙉⚡️     \n╚════════════╝ \n"
+        f"      ╔════════════╗\n     **__⚡️BANTUAN⚡️__**     \n╚════════════╝ \n"
         f"**Hai King {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "═⎆ Pemilik : [King Apis](t.me/PacarFerdilla) \n"
-        "═⎆ Repo    : [Repo](https://github.com/apisuserbot/King-Userbot) \n"
-        "═⎆ Instragam : [Instagram King Apis](Instagram.com/apis_goodboy) \n"
+        "═⎆ developer  : [King Apis](t.me/PacarFerdilla) \n"
+        "═⎆ Repository : [King-Userbot](https://github.com/apisuserbot/King-Userbot) \n"
+        "═⎆ Instragam  : [Instagram King Apis](Instagram.com/apis_goodboy) \n"
         "═⎆ Grup Support : [King Userbot Support](https://t.me/KingUserbotSupport)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
-        f"      ╔════════════╗\n  ⚡️𝘿𝘼𝙁𝙏𝘼𝙍 𝙑𝘼𝙍𝙎⚡️     \n╚════════════╝ \n"
-        f"**Disini Daftar Vars Dari King {DEFAULTUSER}:**\n"
+        f"      ╔════════════╗\n  **__⚡️DAFTAR VARS⚡️__**     \n╚════════════╝ \n"
+        f"**Disini Daftar Vars Dari King** {DEFAULTUSER} :\n"
         "═⎆ Daftar Vars : [DAFTAR VARS](https://raw.githubusercontent.com/apisuserbot/King-Userbot/King-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update(
     {
-        "helper": "**✘ Plugin : **`helper`\
+        "helper": "**✘ Plugin :** `Helper`\
         \n\n  •  **Perintah :** `.khelp`\
-        \n  •  **Function : **Bantuan Untuk ⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️.\
+        \n  •  **Function : **Bantuan Untuk ⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️\
         \n\n  •  **Perintah :** `.vars`\
-        \n  •  **Function : **Melihat Daftar Vars.\
-        \n\n  •  **Perintah :** `.repo`\
-        \n  •  **Function : **Melihat Repo ⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️.\
-        \n\n  •  **Perintah :** `.string`\
-        \n  •  **Function : **Link untuk mengambil String ⚡️𝗞𝗶𝗻𝗴-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡️.\
+        \n  •  **Function : **Melihat Daftar Vars\
     "
     }
 )
