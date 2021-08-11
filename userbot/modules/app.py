66a7df17e0abf4d4fafb4c61ab158e197cb0f26f
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-12 05:14:21 +0700

    created: app

---
+++ userbot/modules/app.py
@@ -0,0 +1,88 @@
+# Thanks Sandy
+# Recode Apis
+
+import bs4
+import requests
+
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register 
+from userbot.utils import edit_or_reply
+
+
+
+@register(outgoing=True, pattern="^.app(?: |$)(.*)")
+async def app_search(event):
+    app_name = event.pattern_match.group(1)
+    event = await edit_or_reply(event, "`Searching!..`")
+    try:
+        remove_space = app_name.split(" ")
+        final_name = "+".join(remove_space)
+        page = requests.get(
+            "https://play.google.com/store/search?q=" + final_name + "&c=apps"
+        )
+        str(page.status_code)
+        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
+        results = soup.findAll("div", "ZmHEEd")
+        app_name = (
+            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
+        )
+        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
+        app_dev_link = (
+            "https://play.google.com"
+            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
+        )
+        app_rating = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "pf5lIe")
+            .find("div")["aria-label"]
+        )
+        app_link = (
+            "https://play.google.com"
+            + results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "vU6FJ p63iDd")
+            .a["href"]
+        )
+        app_icon = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "uzcko")
+            .img["data-src"]
+        )
+        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
+        app_details += " <b>" + app_name + "</b>"
+        app_details += (
+            "\n\n<code>Developer :</code> <a href='"
+            + app_dev_link
+            + "'>"
+            + app_dev
+            + "</a>"
+        )
+        app_details += "\n<code>Rating :</code> " + app_rating.replace(
+            "Rated ", "⭐ "
+        ).replace(" out of ", "/").replace(" stars", "", 1).replace(
+            " stars", "⭐ "
+        ).replace(
+            "five", "5"
+        )
+        app_details += (
+            "\n<code>Fitur :</code> <a href='"
+            + app_link
+            + "'>Dilihat Dalam PlayStore</a>"
+        )
+        app_details += f"\n\n===> {ALIVE_NAME} <==="
+        await event.edit(app_details, link_preview=True, parse_mode="HTML")
+    except IndexError:
+        await event.edit("Tidak ada hasil yang ditemukan dalam pencarian, Silahkan mencari app **Yang Valid**")
+    except Exception as err:
+        await event.edit("Pengecualian Terjadi :- " + str(err))
+
+CMD_HELP.update(
+    {
+        "app": "**Plugin :** `App`\
+        \n\n  •  **Perintah :** `.app`\
+        \n  •  **Function : **Untuk Mencari App Di PlayStore\
+    "
+    }
+)
