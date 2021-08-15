bae8a6ddf7e921efb5dcdd235a1b5776a5848f31
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-15 17:42:43 +0700

    fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -674,6 +674,21 @@ with king:
                 )
             await event.answer([result] if result else None)
 
+        @king.tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+        async def inline_handler(event):
+            builder = event.builder
+            result = None
+            query = event.text
+            if event.query.user_id == uid and query.startswith(
+                    "@RepoMe"):
+                _result = repo_inline()
+                photo_repo = logo
+                result = builder.photo(photo_repo,
+                                       link_preview=False,
+                                       text=_result[0],
+                                       buttons=_result[1],
+                                       )
+
 # =============================================Button========================================= #
 
         @king.tgbot.on(
