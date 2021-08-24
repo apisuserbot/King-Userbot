a0c15fabb58861b5739f10c47c37b49019395e7a
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-24 17:34:23 +0700

    added: animasi 4

---
+++ userbot/modules/animasi4.py
@@ -0,0 +1,43 @@
+# Animasi 4 file
+# Created by Apis
+
+import sleep
+
+from userbot import CMD_HELP 
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.bacot(?: |$)(.*)")
+async def bacot(event):
+    await event.edit("**BACOT LUUU**")
+
+
+@register(outgoing=True, pattern="^.sokap(?: |$)(.*)")
+async def sokap(event):
+    await event.edit("**SOKAP BAT JADI ORANG YA ANJING**")
+
+
+@register(outgoing=True, pattern="^.woi(?: |$)(.*)")
+async def woi(event):
+    await event.edit("**WOI LU SEMUA**")
+
+
+@register(outgoing=True, pattern="^.ngatur(?: |$)(.*)")
+async def ngatur(event):
+     await event.edit("**WOI ANJING , DENGER YA , JADI ORANG GAK USAH NGATUR NGATUR HIDUP ORANG YA NGENTOT , URUS AJA HIDUP LU SENDIRI BANGSAT , UDAH BENER APA KAGAK**")
+
+
+CMD_HELP.update(
+    {
+        "roasting": "**✘ Plugin :** `Roasting`\
+        \n\n  •  **Perintah :** `.bacot`\
+        \n  •  **Function : **Animasi Bacot\
+        \n\n  •  **Perintah :** `.sokap`\
+        \n  •  **Function : **Animasi Sokap\
+        \n\n  •  **Perintah :** `.woi`\
+        \n  •  **Function : **Animasi Woi\
+        \n\n  •  **Perintah :** `.ngatur`\
+        \n  •  **Function : **Animasi Ngatur\
+    "
+    }
+)
