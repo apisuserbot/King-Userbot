d9bac93cc088c1bbff5520570cf0bd2462a669f7
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-29 06:53:51 +0700

    fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -214,6 +214,10 @@ ALIVE_LOGO = os.environ.get(
 INLINE_PIC = os.environ.get(
     "INLINE_PIC") or "https://telegra.ph/file/8eb368517a8d3933c05d9.jpg"
 
+# Default Emoji Help Inline
+EMOJI_HELP = os.environ.get(
+    "EMOJI_HELP") or "|"
+
 # Last.fm Module
 BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
@@ -387,7 +391,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
+        custom.Button.inline(f"{} {} {EMOJI_HELP}".format("{EMOJI_HELP}", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -848,7 +852,7 @@ with king:
                     buttons=[
                         [custom.Button.inline("Menu Pengaturan", data="settings")],
                         [custom.Button.inline(
-                            "Menu Set Vars" data="menu_vars")],
+                            "Menu Set Vars", data="menu_vars")],
                         [custom.Button.inline("Menu Buka", data="opener")],
                         [custom.Button.inline("Menu Tutup", b"close")],
                     ]
