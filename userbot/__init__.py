86ba92458cba42d30a3cec33a427fe0ec1d1cff8
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2021-08-29 06:59:00 +0700

    fixes

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -391,7 +391,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline(f"{} {} {EMOJI_HELP}".format("{EMOJI_HELP}", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} |".format("|", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
