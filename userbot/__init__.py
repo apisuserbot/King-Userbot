4add9b743c590945d776449477363aa42224e03f
ZenitsuXD #M•R•T™ <85629049+ZenitsuXD@users.noreply.github.com>
2021-11-10 10:56:13 +0700

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -125,7 +125,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/apisuserbot/King-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "HatsuneMiku-Userbot")
+    "UPSTREAM_REPO_BRANCH", "King-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
